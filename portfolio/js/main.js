1f8280b9c92981f75d3733b9fefa7ed593d58a01
Nishantjain10 <nishant.jain.775@gmail.com>
2022-03-25 23:36:50 +0530

    Portfolio under development

---
+++ portfolio/js/main.js
@@ -0,0 +1,48 @@
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+); 
+import React from "react";
+import "./style.widgets.skillButton.css";
+
+export default function SkillButton(__buttonProperties) {
+  function clickHandler(e) {
+    e.value = "null";
+  }
+  return (
+    <div className="skill-button-wrapper">
+      <ul className="btn-properties">
+        <div>
+          <li>
+            <a href={clickHandler}>
+              <i className={__buttonProperties.startIcon1}></i>
+              <span>{__buttonProperties.title1}</span>
+            </a>
+          </li>
+          <li>
+            <a href={clickHandler}>
+              <i className={__buttonProperties.startIcon2}></i>
+              <span>{__buttonProperties.title2}</span>
+            </a>
+          </li>
+        </div>
+        <div>
+          <li>
+            <a href={clickHandler}>
+              <i className={__buttonProperties.startIcon3}></i>
+              <span>{__buttonProperties.title3}</span>
+            </a>
+          </li>
+          <li>
+            <a href={clickHandler}>
+              <i className={__buttonProperties.startIcon4}></i>
+              <span className="lastTitle">{__buttonProperties.title4}</span>
+            </a>
+          </li>
+        </div>
+      </ul>
+    </div>
+  );
+}
