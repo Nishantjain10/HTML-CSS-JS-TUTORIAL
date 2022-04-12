b4da319e18da6b7b04be1e1565a9831e9b299e30
Nishantjain10 <nishant.jain.775@gmail.com>
2022-04-12 21:22:48 +0530

    Learning  functions

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,2 +1,41 @@
-let myName = 'Nishant';
-console.log(myName);
+// let isApproved = false; / / Boolean Literal
+// let firstName = undefined;
+// let selectedColor = null; 
+
+
+//objects
+// let person = {
+//     myName: 'Nishant',
+//     age: 30
+// };
+
+//Dot Notation
+// person.myName = 'John';
+  
+// Bracket Notation
+// let selection = 'myName';
+// person[selection] = 'Mary';
+
+// console.log(person.myName);
+
+
+//Arrays
+// let selectedColors = ['red' , 'blue'];
+// selectedColors[2] = 1;
+// console.log(selectedColors);
+
+//Functions 
+// Performing a task
+function greet(name, lastName) {
+    console.log('Hello ' + name + ' ' + lastName);
+}
+
+//Calculating a Value
+function square (number) {
+   return number * number;
+}
+
+console.log(square(2)); 
+
+
+// greet('John', 'Smith');
--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -4,6 +4,27 @@ collapsibles.forEach((item) =>
     this.classList.toggle("collapsible--expanded");
   })
 ); 
+
+const options = {
+  bottom: '64px', // default: '32px'
+  right: 'unset', // default: '32px'
+  left: '32px', // default: 'unset'
+  time: '0.5s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#fff',  // default: '#fff'
+  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: false, // default: true,
+  label: '🌓', // default: ''
+  autoMatchOsTheme: true // default: true
+}
+
+const darkmode =  new Darkmode();
+darkmode.toggle();
+console.log(darkmode.isActivated()) // will return true
+
+
+
 import React from "react";
 import "./style.widgets.skillButton.css";
 
