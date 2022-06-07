22c0dbd2bf45895618034b00f8dd8bf051394531
Nishant Jain <nishant.jain.775@gmail.com>
2022-06-07 23:25:24 +0530

    object-oriented-programming in javascript

---
+++ js-basics/oop.js
@@ -0,0 +1,10 @@
+//objectOrientedProgramming    
+// let employee = {
+//     baseSalary: 30_000,
+//     overtime: 10,
+//     rate: 20,
+//     getWage: function() {
+//         return this.baseSalary + (this.overtime * this.rate)
+//     }
+// };
+// console.log(employee.getWage());
