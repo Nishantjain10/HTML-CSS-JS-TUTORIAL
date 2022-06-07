461d3d12b90a59a4eecc2de391df90e32c1f8117
Nishant Jain <nishant.jain.775@gmail.com>
2022-06-07 23:23:56 +0530

    Encapsulation in javascript

---
+++ js-basics/encapsulation.js
@@ -0,0 +1,9 @@
+//OOP>>Encapsulation-->
+//procedular programming
+    // let baseSalary = 30_000;
+    // let overTime = 10;
+    // let rate = 20;
+
+    // function getWage(baseSalary, overTime, rate) {
+    //     return baseSalary + (overTime * rate);
+    // }
