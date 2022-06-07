435d39c8097d8a5e16f7e67a1f862a9787237009
Nishant Jain <nishant.jain.775@gmail.com>
2022-06-07 23:20:59 +0530

    Arrays in javascript

---
+++ js-basics/arrays.js
@@ -0,0 +1,10 @@
+//Arrays
+// let selectedColors = ['red' , 'blue'];
+// selectedColors[2] = 1;
+// console.log(selectedColors);
+
+//Functions 
+// Performing a task
+// function greet(name, lastName) {
+//     console.log('Hello ' + name + ' ' + lastName);
+// }
