6c4c2651fe33ce12c4ca8864660e8c16956c041a
Nishant Jain <nishant.jain.775@gmail.com>
2022-06-07 23:18:29 +0530

    Objects in Java Script

---
+++ js-basics/objects.js
@@ -0,0 +1,14 @@
+//objects
+// let person = {
+//     myName: 'Nishant',
+//     age: 30
+// };
+
+//Dot Notation
+// person.myName = 'John';
+  
+// Bracket Notation
+// let selection = 'myName';
+// person[selection] = 'Mary';
+
+// console.log(person.myName);
