e5a1923d4b6f1bac0bf39ddd4ca44d94db8a9c53
Nishant Jain <nishant.jain.775@gmail.com>
2022-06-07 23:22:31 +0530

    Functions in javascript

---
+++ js-basics/functions.js
@@ -0,0 +1,15 @@
+//Functions 
+// Performing a task
+// function greet(name, lastName) {
+//     console.log('Hello ' + name + ' ' + lastName);
+// }
+
+// //Calculating a Value
+// function square (number) {
+//    return number * number;
+// }
+
+// console.log(square(2)); 
+
+
+// greet('John', 'Smith');
