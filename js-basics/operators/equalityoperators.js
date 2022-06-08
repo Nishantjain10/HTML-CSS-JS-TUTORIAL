0c9347ca193586626c4691a9244410abf4ae192d
Nishant Jain <nishant.jain.775@gmail.com>
2022-06-08 23:45:22 +0530

    Equality Operators

---
+++ js-basics/operators/equalityoperators.js
@@ -0,0 +1,13 @@
+//equality
+// console.log(x === 1);
+// console.log(x !== 1);
+
+
+//EQUALITY Operators
+// console.log (1 === 1); //strict equality (Type+Value) true
+// console.log ('1' === 1) //false (different type and value!)
+
+//lose equality
+// console.log(1 == 1); //true (Only Value Verification!)
+// console.log('1' == 1); //true (Will convert 1 to a string!)
+// console.log(true == 1); //true (will convert 1 to a boolean!)
