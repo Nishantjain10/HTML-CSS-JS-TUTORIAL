4edaba6535c0e9d372c7ea78d3291da4a29782fe
Nishant Jain <nishant.jain.775@gmail.com>
2022-06-08 23:46:54 +0530

    Ternary Operators

---
+++ js-basics/operators/ternaryoperators.js
@@ -0,0 +1,9 @@
+//TERNARY Operators
+//If a customer has 100 points
+//He is a gold member
+//Else silver
+
+// let points = 110;
+// let type = points > 100 ? 'gold' : 'silver'; // validate (point>100) ?(if true) gold(then gold) else silver.
+
+// console.log(type)
