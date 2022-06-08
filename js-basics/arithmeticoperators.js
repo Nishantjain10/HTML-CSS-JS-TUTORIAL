debd593a29161264b483d8325d0bed04d8426543
Nishant Jain <nishant.jain.775@gmail.com>
2022-06-08 23:41:26 +0530

    Arithmetic Operators

---
+++ js-basics/arithmeticoperators.js
@@ -0,0 +1,17 @@
+//OPERATORS
+//Arithmetic Operators
+//   let x = 10;
+//   let y = 2;
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x % y);
+// console.log(x ** y); //square
+
+//increment 
+// console.log(++x)
+
+//decrement
+// console.log(x++)
+// console.log(x)
