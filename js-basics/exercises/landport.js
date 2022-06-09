9c57f1341811cbcb20f23003c5cda134ce6d06fa
Nishant Jain <nishant.jain.775@gmail.com>
2022-06-09 13:52:07 +0530

    Landscape Portrait Exercise

---
+++ js-basics/exercises/landport.js
@@ -0,0 +1,8 @@
+//exercise-landscape/Portrait
+// let checkCondition = isLandscape(40,56)
+// console.log(checkCondition);
+// console.log(isLandscape(800, 900));
+
+// function isLandscape(width, height) {
+//   return (width > height);  
+// }
