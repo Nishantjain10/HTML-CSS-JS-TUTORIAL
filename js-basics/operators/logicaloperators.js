fb5da36c4bc5a98f1de6896e805613fcfafb70bf
Nishant Jain <nishant.jain.775@gmail.com>
2022-06-09 13:46:38 +0530

    Logical Operators

---
+++ js-basics/operators/logicaloperators.js
@@ -0,0 +1,43 @@
+//LOGICAL Operators
+//logical AND
+//logical OR
+//logical Not
+
+//Example
+//If a user has good credits score
+//High income
+//User is eligible for a loan.
+
+//logical And (&&)
+//return true if both operands are true
+// console.log(true && true);  //true
+// console.log(true && false); //false as one operand is not true.
+
+// let highIncome = true;
+// let goodCreditScore  = true;
+// let eligibleForLoan = (highIncome && goodCreditScore);
+// console.log(eligibleForLoan)
+
+
+//logical OR (||)
+//return true if one operand is true
+// console.log(true || false); //true, as one value is true!
+
+// let highIncome = true;
+// let goodCreditScore  = false;
+// let eligibleForLoan = (highIncome || goodCreditScore);
+// console.log(eligibleForLoan)
+
+
+//logical NOT (!)
+//return if both values are false.
+//return if not eligible for loan refuse application 
+
+
+// let highIncome = false;
+// let goodCreditScore  = false;
+// let eligibleForLoan = (highIncome || goodCreditScore);
+// let applicationRefuse = !eligibleForLoan;
+
+// console.log('Eligible for loan', eligibleForLoan)
+// console.log('Application Refused', applicationRefuse)
