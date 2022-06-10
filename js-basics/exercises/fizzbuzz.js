8d2e0d1b33afc7427cb99a05b41b8e93d5816492
Nishant Jain <nishant.jain.775@gmail.com>
2022-06-10 23:42:23 +0530

    Fizzbuzz Exercise

---
+++ js-basics/exercises/fizzbuzz.js
@@ -0,0 +1,18 @@
+//exercise-FizzBuzz
+// console.log(fizzBuzz(false));
+
+// function fizzBuzz(input) {
+//     if(typeof input !== 'number')
+//     return NaN;
+    
+//     if((input % 3 === 0) && (input % 5 === 0))
+//     return('fizzbuzz');
+    
+//     if(input % 3 === 0) 
+//     return('fizz');
+
+//     if(input % 5 === 0)
+//     return('buzz');
+    
+//     return(input)
+// }
