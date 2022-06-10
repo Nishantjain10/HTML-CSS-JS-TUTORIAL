08efef104ed599d5d5238ba3d82b80e9973c7e83
Nishant Jain <nishant.jain.775@gmail.com>
2022-06-10 23:44:53 +0530

    Truthy Check Array Exercise

--- js-basics/index.js
+++ js-basics/index.js
@@ -241,15 +241,15 @@
 
 //exercise-arrayTruthyCheckValues 
 
-const array = [0, null, undefined, '', 2, 3];
+// const array = [0, null, undefined, '', 2, 3];
 
-console.log(countTruthy(array));
+// console.log(countTruthy(array));
 
-function countTruthy(array) {
-    let count = 0;
-    for(let value of array)
-    if(value)
-    count++;
-    return count;
-} 
+// function countTruthy(array) {
+//     let count = 0;
+//     for(let value of array)
+//     if(value)
+//     count++;
+//     return count;
+// } 
 
