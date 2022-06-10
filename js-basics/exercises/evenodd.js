5f709da5b8b63c8c3bbdc0ca5443ad0f785ade12
Nishant Jain <nishant.jain.775@gmail.com>
2022-06-10 23:43:31 +0530

    EvenOdd Exercise

---
+++ js-basics/exercises/evenodd.js
@@ -0,0 +1,13 @@
+//exercise-evenOdd Numbers
+// showNumbers(10)
+    
+// function showNumbers(limit) {
+//     // for (let i = 0; i <= limit; i++) {
+//     //     if(i % 2 === 0) console.log(i, ' Even');
+//     //     else console.log(i, 'Odd');
+
+
+//     const message = i % 2 ===0 ? 'EVEN': 'ODD'
+//     console.log(i, message)
+//     }  
+// }
