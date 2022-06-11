4802ddf5b861e2619a75ea6d5082b10a2bb63bfc
Nishant Jain <nishant.jain.775@gmail.com>
2022-06-11 22:58:56 +0530

    String Property Exercise

---
+++ js-basics/exercises/stringproperty.js
@@ -0,0 +1,15 @@
+// const employee = {
+//     empName: 'Jones',
+//     empSalary: 50000,
+//     empCompany: 'Starbucks' ,
+//     empContact: 9109000011 
+// };
+
+// showEmployeeDetails(employee);
+
+// function showEmployeeDetails(obj) {
+//     for (let key in obj) {
+//      if(typeof obj[key] === 'string')
+//      console.log(key, obj[key])
+//     }
+// }
