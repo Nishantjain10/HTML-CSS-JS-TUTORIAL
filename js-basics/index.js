78efb3a1679d05884ed6b92746542e482b10cad1
Nishant Jain <nishant.jain.775@gmail.com>
2022-06-11 23:25:06 +0530

    Sum limit Exercise

--- js-basics/index.js
+++ js-basics/index.js
@@ -272,14 +272,14 @@
 
 
 // exercise-sumLimits
-console.log(sum(10));
+// console.log(sum(10));
 
-function sum(limit) {
- let sum = 0;
+// function sum(limit) {
+//  let sum = 0;
 
-      for (let i = 1; i <= limit; i++) 
-       if (i % 3 === 0 || i % 5 === 0)
-       sum += i;
+//       for (let i = 1; i <= limit; i++) 
+//        if (i % 3 === 0 || i % 5 === 0)
+//        sum += i;
 
-    return sum;
-}
+//     return sum;
+// }
