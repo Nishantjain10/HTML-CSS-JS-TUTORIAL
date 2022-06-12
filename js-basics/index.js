54ac5e33d542086c5c9db76d7e14fa34c7f8689c
Nishant Jain <nishant.jain.775@gmail.com>
2022-06-12 23:40:12 +0530

    Average Grade Exercise

--- js-basics/index.js
+++ js-basics/index.js
@@ -291,11 +291,7 @@ const marks = [80,80,80]
 console.log(calculateGrade(marks));
 
 function calculateGrade(marks) {
-     let sum = 0;
-     for (let mark of marks)   
-        sum += mark;
-     let average = sum / marks.length;
-
+    const average = calculateAverage(marks)
      if (average < 60) return 'F';
      if (average < 70) return 'D';
      if (average < 80) return 'C';
