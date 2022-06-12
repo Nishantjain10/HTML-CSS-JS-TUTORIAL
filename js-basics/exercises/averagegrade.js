47dd49e130cf795ea61799ad7a4151288341d3fb
Nishant Jain <nishant.jain.775@gmail.com>
2022-06-12 23:43:41 +0530

    Average Grade Exercise

--- js-basics/exercises/averagegrade.js
+++ js-basics/exercises/averagegrade.js
@@ -1,18 +1,18 @@
-const marks = [80,80,80]
-console.log(calculateGrade(marks));
+// const marks = [80,80,50]
+// console.log(calculateGrade(marks));
 
-function calculateGrade(marks) {
-    const average = calculateAverage(marks);
-     if (average < 60) return 'F';
-     if (average < 70) return 'D';
-     if (average < 80) return 'C';
-     if (average < 90) return 'D';
-     return 'A';
-}
+// function calculateGrade(marks) {
+//     const average = calculateAverage(marks);
+//      if (average < 60) return 'F';
+//      if (average < 70) return 'D';
+//      if (average < 80) return 'C';
+//      if (average < 90) return 'D';
+//      return 'A';
+// }
 
-function calculateAverage(array) {
-    let sum = 0;
-    for (let value of array)   
-       sum += value;
-    return sum / array.length;
-}
+// function calculateAverage(array) {
+//     let sum = 0;
+//     for (let value of array)   
+//        sum += value;
+//     return sum / array.length;
+// }
