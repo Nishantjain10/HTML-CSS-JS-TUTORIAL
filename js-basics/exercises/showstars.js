a241ad01d0ace4ab58dd3f4137f6d8733f99dd26
Nishant Jain <nishant.jain.775@gmail.com>
2022-06-14 23:04:33 +0530

    Show Stars Exercise

--- js-basics/exercises/showstars.js
+++ js-basics/exercises/showstars.js
@@ -1,10 +1,10 @@
-showStars(6);
+// showStars(6);
 
-function showStars(rows) {
-    for(let row = 1; row <= rows; row++) {
-        let pattern = '';
-        for(let i = 0; i < row; i++)
-        pattern += '*'
-        console.log(pattern);
-    }
-}
+// function showStars(rows) {
+//     for(let row = 1; row <= rows; row++) {
+//         let pattern = '';
+//         for(let i = 0; i < row; i++)
+//         pattern += '*'
+//         console.log(pattern);
+//     }
+// }
