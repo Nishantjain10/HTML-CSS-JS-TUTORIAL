677568bc1b927e50af195f5e69cf1d532728139c
Nishant Jain <nishant.jain.775@gmail.com>
2022-06-15 22:18:20 +0530

    Show Prime Numbers Exercise

--- js-basics/index.js
+++ js-basics/index.js
@@ -322,6 +322,21 @@
 showPrimes(20);
 
 function showPrimes(limit) {
-     for(let i = 1; i <= limit; i++)
-        if
+     for(let number = 2; number <= limit; number++) {
+        let isPrime = true;
+        for (let factor = 2; factor < number; factor++) {
+           if (number % factor === 0) {
+            isPrime = false;
+            break;
+           } 
+        }
+
+
+        if (isPrime) console.log(number);
+
+
+
+        // if (i % 1 === 0 || i % i === 0)
+        // console.log(i);
+    }
 }
