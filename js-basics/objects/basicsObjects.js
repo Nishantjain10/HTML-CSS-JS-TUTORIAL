266a1de1bb59950de63efc2a300fcc6e57844630
Nishant Jain <nishant.jain.775@gmail.com>
2022-06-16 23:14:50 +0530

    Learning About Objects In JavaScript

---
+++ js-basics/objects/basicsObjects.js
@@ -0,0 +1,15 @@
+//Object-oriented Programming (OOP)
+
+const circle = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1,
+    },
+    isVisible: true,
+    draw: function() {
+        console.log('draw')
+    }
+}
+
+circle.draw() //method
