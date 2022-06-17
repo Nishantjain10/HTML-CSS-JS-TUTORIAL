4a82a41fd1877cb3f5d0d63825de16a474b0e25c
Nishant Jain <nishant.jain.775@gmail.com>
2022-06-17 22:54:35 +0530

    Factory Functions in Javascript

--- js-basics/index.js
+++ js-basics/index.js
@@ -363,19 +363,17 @@
 
 
 // Factory Functions
- const circle = {
-     radius: 1,
-     location: {
-         x: 1,
-         y: 1,
-     },
-     isVisible: true,
-     draw: function() {
-        console.log('draw')
-     }
- }
 
-// circle.draw() //method
-
-// function draw() {}
-// function move() {}
+function createCircle() {
+    return {
+        radius: 1,
+        location: {
+            x: 1,
+            y: 1
+        },
+        isVisible: true,
+        draw: function() {
+           console.log('draw');
+        }
+    };
+}
