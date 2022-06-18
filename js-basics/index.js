c24727d71c43221d7750994ccbc5a9982dc684ea
Nishant Jain <nishant.jain.775@gmail.com>
2022-06-18 22:37:40 +0530

    Constructor Function In Javascript

--- js-basics/index.js
+++ js-basics/index.js
@@ -386,15 +386,15 @@
 //Pascal Notation: OneTwoThreeFour
 
 // Constructor Function
-function Circle(radius) {
-    this.radius = radius;
-    this.draw = function() {
-        console.log('draw');
-    }
-}
+// function Circle(radius) {
+//     this.radius = radius;
+//     this.draw = function() {
+//         console.log('draw');
+//     }
+// }
 
-const circle = new Circle(1); //use 'new' in constructor function
-                              //we use 'this' in constructor function
-                              //Pascal Notation is used in constructor function!
-                              //familiar to developers in c#,java
+// const circle = new Circle(1); //use 'new' in constructor function
+//                               //we use 'this' in constructor function
+//                               //Pascal Notation is used in constructor function!
+//                               //familiar to developers in c#,java
                               
