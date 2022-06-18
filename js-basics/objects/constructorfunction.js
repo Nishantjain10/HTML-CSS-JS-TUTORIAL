cad33e9e23494da1c4ecf1fe6376a4964ef5d51b
Nishant Jain <nishant.jain.775@gmail.com>
2022-06-18 22:37:07 +0530

    Constructor Function In Javascript

---
+++ js-basics/objects/constructorfunction.js
@@ -0,0 +1,11 @@
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function() {
+        console.log('draw');
+    }
+}
+
+const circle = new Circle(1); //use 'new' in constructor function
+                              //we use 'this' in constructor function
+                              //Pascal Notation is used in constructor function!
+                              //familiar to developers in c#,java
