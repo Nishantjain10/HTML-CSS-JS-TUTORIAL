5116e6b1fd10632bc5f30112d43b44db2e1cca32
Nishant Jain <nishant.jain.775@gmail.com>
2022-06-19 22:53:08 +0530

    Dynamic Nature Of Objects In Javascript

---
+++ js-basics/objects/dynamicobjects.js
@@ -0,0 +1,13 @@
+const circle = {
+    radius: 1
+};
+
+//cannot reassign constant circle = {}
+
+circle.color = 'yellow'
+circle.draw = function() {}
+
+delete circle.color;
+delete circle.draw;
+
+console.log(circle);
