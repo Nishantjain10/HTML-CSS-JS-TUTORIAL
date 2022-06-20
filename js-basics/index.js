d5e779f4341eef2a5fb3d555599eec3b708bd972
Nishant Jain <nishant.jain.775@gmail.com>
2022-06-20 23:28:42 +0530

    Constructor Property

--- js-basics/index.js
+++ js-basics/index.js
@@ -417,41 +417,41 @@
 
 
 
-//Constructor Property
+// //Constructor Property
 
-//factory function
-function createCircle(radius) {
-         return {
-             radius,
+// //factory function
+// function createCircle(radius) {
+//          return {
+//              radius,
     
-             draw() {
-                 console.log('draw');
-             }
-         };
- }
+//              draw() {
+//                  console.log('draw');
+//              }
+//          };
+//  }
 
- const circle = createCircle(1);
+//  const circle = createCircle(1);
 
- //constructor function
- function Circle(radius) {
-    this.radius = radius;
-    this.draw = function() {
-        console.log('draw');
-    }
-}
+//  //constructor function
+//  function Circle(radius) {
+//     this.radius = radius;
+//     this.draw = function() {
+//         console.log('draw');
+//     }
+// }
 
-    const another = new Circle(1);
+//     const another = new Circle(1);
 
 
 
-//circle.constructor 
-//--{Built in javascript constructor}
+// //circle.constructor 
+// //--{Built in javascript constructor}
 
-let x = {};
+// let x = {};
 
-//let x = new Object();
+// //let x = new Object();
 
-new String(); //'', "", ``
-new Boolean(); //true,false
-new Number(); //1,2,3
+// new String(); //'', "", ``
+// new Boolean(); //true,false
+// new Number(); //1,2,3
 
