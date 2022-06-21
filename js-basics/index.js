468c27938fe800ce22214fb674dc925f27d0a41c
Nishant Jain <nishant.jain.775@gmail.com>
2022-06-21 23:16:37 +0530

    Functions Are Objects

--- js-basics/index.js
+++ js-basics/index.js
@@ -459,30 +459,30 @@
 
 //Functions Are Object
 
-//constructor function
-  function Circle(radius) {
-     this.radius = radius;
-     this.draw = function() {
-         console.log('draw');
-     }
- }
-
-//circle.name
-//circle.length
-//circle.constructor
-
-//  const Circle1 = new Function('radius', `
-//  this.radius = radius;
-//  this.draw = function() {
-//      console.log('draw');
+// //constructor function
+//   function Circle(radius) {
+//      this.radius = radius;
+//      this.draw = function() {
+//          console.log('draw');
+//      }
 //  }
-//  `);
 
-//  const circle = new Circle1(1);
+// //circle.name
+// //circle.length
+// //circle.constructor
 
-Circle.call({}, 1)   //--> new Circle(1);
-                     // {} --> specifies target of this
+// //  const Circle1 = new Function('radius', `
+// //  this.radius = radius;
+// //  this.draw = function() {
+// //      console.log('draw');
+// //  }
+// //  `);
 
-Circle.apply({}, [1, 2, 3])  //apply is used for array
+// //  const circle = new Circle1(1);
 
- const another = new Circle(1);
+// Circle.call({}, 1)   //--> new Circle(1);
+//                      // {} --> specifies target of this
+
+// Circle.apply({}, [1, 2, 3])  //apply is used for array
+
+//  const another = new Circle(1);
