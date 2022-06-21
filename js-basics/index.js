0b59f5345c332a304ca431c366c9a82989e5a704
Nishant Jain <nishant.jain.775@gmail.com>
2022-06-21 23:10:54 +0530

    Functions Are Objects

--- js-basics/index.js
+++ js-basics/index.js
@@ -467,4 +467,13 @@
      }
  }
 
+ const Circle1 = new Function('radius', `
+ this.radius = radius;
+ this.draw = function() {
+     console.log('draw');
+ }
+ `);
+
+ const circle = new Circle(1);
+
  const another = new Circle(1);
