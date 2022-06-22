d8ad9a117c58477f5cb5de82766bf805d34b2e70
Nishant Jain <nishant.jain.775@gmail.com>
2022-06-22 22:57:10 +0530

    Value v/s Reference Type In Javascript

---
+++ js-basics/objects/valuereference.js
@@ -0,0 +1,24 @@
+//Value(Primitives)         //Reference Types(Objects)
+//-->Number                 //-->Number
+//   Boolean                // Function
+//   String                 // Array
+//   Symbol
+//   Undefined
+//   Null
+
+// let x = { value : 10 };
+// let y = x;
+
+// x.value = 20;
+
+//Primitives are copied by their values
+//Objects are copied by their reference
+
+// let obj = { value: 10 }; 
+
+//  function increase(obj) {
+//         obj.value++;
+//         }
+
+//     increase (obj);
+//     console.log(obj);
