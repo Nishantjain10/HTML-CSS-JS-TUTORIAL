e882eef8cafcfda80d767335cb03ddbde6fa190f
Nishant Jain <nishant.jain.775@gmail.com>
2022-06-23 21:17:20 +0530

    Enumeration Properties of an Object in JavaScript

--- js-basics/index.js
+++ js-basics/index.js
@@ -517,3 +517,16 @@
 
 
 //Enumeration Properties of an Object!
+
+const circle = {
+    radius: 1,
+    draw() {
+        console.log('draw')
+    }
+};
+
+for (let key in circle)
+    console.log(key. circle[key]);
+
+for (let key of circle)
+    console.log(key);
