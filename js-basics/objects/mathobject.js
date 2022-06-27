146c940dd5f24a579cd049f1f958cec243a7d486
Nishant Jain <nishant.jain.775@gmail.com>
2022-06-27 22:19:36 +0530

    Math Object In JavaScript

---
+++ js-basics/objects/mathobject.js
@@ -0,0 +1,15 @@
+//Math Object 
+//Math is a built-in object that has properties and methods for mathematical constants and functions. It's not a function object.
+
+// function getRandomArbitrary(min, max) {
+//     return Math.random() * (max - min) + min;
+//   }
+  
+//math.round(1.9)
+//output - 2
+
+//math.max(1,2,3,4,5)
+//output - 5
+
+//math.min(1,2,3,4,5)
+//output - 1
