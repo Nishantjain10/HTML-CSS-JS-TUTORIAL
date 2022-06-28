40e02389a205d81c29dd6172342b4e91a0f4d485
Nishant Jain <nishant.jain.775@gmail.com>
2022-06-28 22:01:36 +0530

    String Object In JavaScript

--- js-basics/objects/mathobject.js
+++ js-basics/objects/mathobject.js
@@ -21,7 +21,15 @@
 //String Primitive
 const message = 'This is my first message';
 //message.length
-
+//message[0] ---> T
+//message.includes('my' --> true)
+//message.startsWith('This') --> True
+//message.startsWith('this') --> True
+//message.endsWith('e') ---> True
+//message.indexOf('my') --> 8
+//message.replace('first','second')  --> This is my second message
+//message.toUpperCase()
+//message.trim()  gets rid of all whitespace before and after our message
 
 //String Object
 const another = new String('hi')
