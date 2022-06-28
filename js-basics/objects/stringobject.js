78278eb21cfc487342b74f57778daf3219c27959
Nishant Jain <nishant.jain.775@gmail.com>
2022-06-28 22:05:09 +0530

    String Object In JavaScript

---
+++ js-basics/objects/stringobject.js
@@ -0,0 +1,20 @@
+//String Primitive
+const message = ' This is my first message ';
+//message.length
+//message[0] ---> T
+//message.includes('my' --> true)
+//message.startsWith('This') --> True
+//message.startsWith('this') --> True
+//message.endsWith('e') ---> True
+//message.indexOf('my') --> 8
+//message.replace('first','second')  --> This is my second message
+//message.toUpperCase()
+//message.trim()  gets rid of all whitespace before and after our message
+//message.split(' ')
+
+//String Object
+const another = new String('hi') 
+
+
+// const message = ' This is my \'first message';
+// const message = ' This is my\n first message';
