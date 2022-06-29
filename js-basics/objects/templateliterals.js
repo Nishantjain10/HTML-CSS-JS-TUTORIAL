62f665b9346675a12ea22e6ac11b87496f4b685c
Nishant Jain <nishant.jain.775@gmail.com>
2022-06-29 18:16:51 +0530

    Template Literals In JavaScript

---
+++ js-basics/objects/templateliterals.js
@@ -0,0 +1,14 @@
+const message = ' This is my\n first message';
+const employeeName = 'John';
+
+console.log(message);
+
+const another = 
+`Hi ${employeeName} ${2 + 3},
+
+Thank you for joining in my mailing list.
+
+Regard,
+Mosh`;
+
+console.log(another);
