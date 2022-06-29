d227420c1b6456a2e9c5f839ba86ab77af62ab80
Nishant Jain <nishant.jain.775@gmail.com>
2022-06-29 18:16:19 +0530

    Template Literals In JavaScript

--- js-basics/index.js
+++ js-basics/index.js
@@ -615,8 +615,16 @@
 //Template Literals ES6 ``
 
 const message = ' This is my\n first message';
+const employeeName = 'John';
 
+console.log(message);
 
 const another = 
-`This is my 
-'first' message`;
+`Hi ${employeeName} ${2 + 3},
+
+Thank you for joining in my mailing list.
+
+Regard,
+Mosh`;
+
+console.log(another);
