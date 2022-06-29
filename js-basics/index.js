e124dc5e941336d995886a858bac7397f65bdf2b
Nishant Jain <nishant.jain.775@gmail.com>
2022-06-29 17:29:17 +0530

    Template Literals In JavaScript

--- js-basics/index.js
+++ js-basics/index.js
@@ -582,3 +582,41 @@
 
 //math.min(1,2,3,4,5)
 //output - 1
+
+
+
+
+// String Object
+
+// //String Primitive
+// const message = ' This is my first message ';
+// //message.length
+// //message[0] ---> T
+// //message.includes('my' --> true)
+// //message.startsWith('This') --> True
+// //message.startsWith('this') --> True
+// //message.endsWith('e') ---> True
+// //message.indexOf('my') --> 8
+// //message.replace('first','second')  --> This is my second message
+// //message.toUpperCase()
+// //message.trim()  gets rid of all whitespace before and after our message
+// //message.split(' ')
+
+// //String Object
+// const another = new String('hi') 
+
+
+// // const message = ' This is my \'first message';
+// // const message = ' This is my\n first message';
+
+
+
+
+//Template Literals ES6 ``
+
+const message = ' This is my\n first message';
+
+
+const another = 
+`This is my 
+'first' message`;
--- js-basics/objects/mathobject.js
+++ js-basics/objects/mathobject.js
@@ -13,28 +13,3 @@
 
 //math.min(1,2,3,4,5)
 //output - 1
-
-
-
-// String Object
-
-// //String Primitive
-// const message = ' This is my first message ';
-// //message.length
-// //message[0] ---> T
-// //message.includes('my' --> true)
-// //message.startsWith('This') --> True
-// //message.startsWith('this') --> True
-// //message.endsWith('e') ---> True
-// //message.indexOf('my') --> 8
-// //message.replace('first','second')  --> This is my second message
-// //message.toUpperCase()
-// //message.trim()  gets rid of all whitespace before and after our message
-// //message.split(' ')
-
-// //String Object
-// const another = new String('hi') 
-
-
-// // const message = ' This is my \'first message';
-// // const message = ' This is my\n first message';
