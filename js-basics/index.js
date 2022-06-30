b99c4b199e4372c9a5fdcd7ab3af7f1abfdb993f
Nishant Jain <nishant.jain.775@gmail.com>
2022-06-30 21:27:31 +0530

    Blog Post Exercise

--- js-basics/index.js
+++ js-basics/index.js
@@ -729,3 +729,18 @@
 
 // console.log(post);
 
+
+
+//Constructor Function Exercise
+let post = new Post('a', 'b', 'c');
+console.log(post);
+
+
+function Post(title, body, author, ) {
+    this.title = title;
+    this.body = body;
+    this.author = author;
+    this.views = 0;
+    this.comments = [];
+    this.isLive = false;
+}
