1d5232e921297a46c158f9ecbb17ffdd4db5fa3d
Nishant Jain <nishant.jain.775@gmail.com>
2022-06-30 21:33:24 +0530

    Constructor Function Exercise

--- js-basics/index.js
+++ js-basics/index.js
@@ -732,15 +732,15 @@
 
 
 //Constructor Function Exercise
-let post = new Post('a', 'b', 'c');
-console.log(post);
+// let post = new Post('a', 'b', 'c');
+// console.log(post);
 
 
-function Post(title, body, author, ) {
-    this.title = title;
-    this.body = body;
-    this.author = author;
-    this.views = 0;
-    this.comments = [];
-    this.isLive = false;
-}
+// function Post(title, body, author, ) {
+//     this.title = title;
+//     this.body = body;
+//     this.author = author;
+//     this.views = 0;
+//     this.comments = [];
+//     this.isLive = false;
+// }
