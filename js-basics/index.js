b25ae016d4dde1fac4773de6b0fa19b0384c9c9a
Nishant Jain <nishant.jain.775@gmail.com>
2022-07-01 19:34:16 +0530

    Price Range Object Exercise

--- js-basics/index.js
+++ js-basics/index.js
@@ -744,3 +744,16 @@
 //     this.comments = [];
 //     this.isLive = false;
 // }
+
+
+
+//Price Range Object Exercise
+let priceRanges = [
+    {label: '$', tooltip: 'Inexpensive', minPerson: 0, maxPerPerson: 10},
+    {label: '$$', tooltip: 'Moderate', minPerson: 11, maxPerPerson: 20},
+    {label: '$$$', tooltip: 'Expensive', minPerson: 21, maxPerPerson: 50}
+];
+
+let restaurants = [
+    {averagePerPerson: 5}
+]
