69f9a7105d31eb11c5c3f440577c16637b042541
Nishant Jain <nishant.jain.775@gmail.com>
2022-07-01 19:34:42 +0530

    Price Range Object Exercise

---
+++ js-basics/objectexercises/pricerange.js
@@ -0,0 +1,9 @@
+let priceRanges = [
+    {label: '$', tooltip: 'Inexpensive', minPerson: 0, maxPerPerson: 10},
+    {label: '$$', tooltip: 'Moderate', minPerson: 11, maxPerPerson: 20},
+    {label: '$$$', tooltip: 'Expensive', minPerson: 21, maxPerPerson: 50}
+];
+
+let restaurants = [
+    {averagePerPerson: 5}
+]
