727439c0719d3fa2e5fe03319cd0e5c40e899c27
Nishant Jain <nishant.jain.775@gmail.com>
2022-07-02 23:17:54 +0530

    Adding elements in an array

--- js-basics/arrays/addelements.js
+++ js-basics/arrays/addelements.js
@@ -1,15 +1,15 @@
-//adding elements in a array
-const numbers = [3, 4];
+// //adding elements in a array
+// const numbers = [3, 4];
 
-//End
-numbers.push(5,6);
-
-
-//Beginning
-numbers.unshift(1,2);
+// //End
+// numbers.push(5,6);
 
 
-//Middle
-numbers.splice(2, 0, 'a', 'b');
+// //Beginning
+// numbers.unshift(1,2);
 
-console.log(numbers);
+
+// //Middle
+// numbers.splice(2, 0, 'a', 'b');
+
+// console.log(numbers);
