e0d44d53ca2ee2ed9a2e42d31187595323667543
Nishant Jain <nishant.jain.775@gmail.com>
2022-07-03 22:56:57 +0530

    Finding Elemets In an Array

--- js-basics/index.js
+++ js-basics/index.js
@@ -778,10 +778,10 @@
 
 
 //Find Elements In an Array
-const numbers = [1, 2, 3, 1, 4];
+// const numbers = [1, 2, 3, 1, 4];
 
-console.log(numbers.indexOf(1, 2));
-console.log(numbers.lastIndexOf(1));
+// console.log(numbers.indexOf(1, 2));
+// console.log(numbers.lastIndexOf(1));
 
-console.log(numbers.indexOf(1) !== -1);
-console.log(numbers.includes(1));
+// console.log(numbers.indexOf(1) !== -1);
+// console.log(numbers.includes(1));
