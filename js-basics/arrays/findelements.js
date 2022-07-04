9de2c4a099be5719aa1be1b0604abc384e398050
Nishant Jain <nishant.jain.775@gmail.com>
2022-07-04 22:49:38 +0530

    Finding Elements Of Reference Type In Array

--- js-basics/arrays/findelements.js
+++ js-basics/arrays/findelements.js
@@ -5,3 +5,24 @@
 
 // console.log(numbers.indexOf(1) !== -1);
 // console.log(numbers.includes(1));
+
+
+
+
+//Find Elements In an Array(Reference Types)
+
+const courses = [
+    { id: 1, courseName: 'a'},
+    { id: 2, courseName: 'b'}
+]
+
+// console.log(courses.includes({id: 1, courseName: 'a' }));
+
+// const course = courses.find(function(course) {
+//     // return course.courseName === 'a';
+//     return course.courseName === 'xyz';
+// })  
+const course = courses.findIndex(function(course) {
+    return course.courseName === 'a';
+    // return course.courseName === 'xyz';
+})  
