a76dab8f15696b72ff6cab860c002370735134b2
Nishant Jain <nishant.jain.775@gmail.com>
2022-07-05 20:49:01 +0530

    arrow function

---
+++ js-basics/arrays/arrowfunction.js
@@ -0,0 +1,9 @@
+const courses = [
+    { id: 1, courseName: 'a'},
+    { id: 2, courseName: 'b'}
+]
+
+const course = courses.find(course => course.courseName === 'a');
+
+
+console.log(course);
--- js-basics/index.js
+++ js-basics/index.js
@@ -811,13 +811,5 @@
 
 
 //Arrow Function 
-const courses = [
-    { id: 1, courseName: 'a'},
-    { id: 2, courseName: 'b'}
-]
 
-const course = courses.find(course => course.courseName === 'a');
-
-
-console.log(course);
 
