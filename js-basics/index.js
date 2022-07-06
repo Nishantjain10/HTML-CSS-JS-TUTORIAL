7c76e96c6ff445da2518da77b998e2e48c6e1a7c
Nishant Jain <nishant.jain.775@gmail.com>
2022-07-06 22:48:46 +0530

    Removing Elements In an Array

--- js-basics/index.js
+++ js-basics/index.js
@@ -828,4 +828,14 @@
 const numbers = [1, 2, 3, 4];
 
 
+//End
+const last = numbers.pop();
+//console.log(last);
 
+//Beginning
+const first = numbers.shift();
+console.log(first);
+
+//Middle
+
+console.log(numbers);
