6b3a6e56235741afc65c60da7ab44bc2376cbb5d
Nishant Jain <nishant.jain.775@gmail.com>
2022-07-07 22:58:08 +0530

    Emptying an Array

--- js-basics/arrays/emptyelem.js
+++ js-basics/arrays/emptyelem.js
@@ -1,18 +1,18 @@
-let numbers = [1, 2, 3, 4];
-let another = numbers;
+// let numbers = [1, 2, 3, 4];
+// let another = numbers;
 
- //Solution 1
-    // numbers = [];
+//  //Solution 1
+//     // numbers = [];
     
- //Solution 2
-    // numbers.length = 0;  //better solution
+//  //Solution 2
+//     // numbers.length = 0;  //better solution
 
- //Solution 3
-    // numbers.splice(0, numbers.length);
+//  //Solution 3
+//     // numbers.splice(0, numbers.length);
 
- //Solution 4
-while (numbers.length > 0)
-    numbers.pop();
+//  //Solution 4
+// while (numbers.length > 0)
+//     numbers.pop();
 
-    console.log(numbers);
-    console.log(another);
+//     console.log(numbers);
+//     console.log(another);
