962fc36bb304c19b85c835a50dc89fcc213eb649
Nishant Jain <nishant.jain.775@gmail.com>
2022-07-07 22:54:57 +0530

    Emptying an Array

--- js-basics/index.js
+++ js-basics/index.js
@@ -843,6 +843,19 @@
 
 
 //Emptying an array
- const numbers = [1, 2, 3, 4];
+let numbers = [1, 2, 3, 4];
+let another = numbers;
 
+ //Solution 1
+    // numbers = [];
+    
+ //Solution 2
+    // numbers.length = 0;
 
+ //Solution 3
+    numbers.splice(0, numbers.length);
+
+ //Solution 4
+    console.log(numbers);
+    console.log(another);
+    
