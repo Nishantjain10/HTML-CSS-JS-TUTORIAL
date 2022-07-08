2907b5dcf96cd825de4a1f3cd44a71a8660ce4de
Nishant Jain <nishant.jain.775@gmail.com>
2022-07-08 23:14:48 +0530

    Combining and Slicing Array

--- js-basics/arrays/combine.js
+++ js-basics/arrays/combine.js
@@ -1,10 +1,13 @@
-const first = [{id: 1}];
-const second = [4, 5, 6];
-
-const combined = first.concat(second);
-first[0].id = 10;
+//Combining And Slicing Array
 
 
-const slice = combined.slice();
-console.log(combined);
-console.log(slice);
+// const first = [{id: 1}];
+// const second = [4, 5, 6];
+
+// const combined = first.concat(second);
+// first[0].id = 10;
+
+
+// const slice = combined.slice();
+// console.log(combined);
+// console.log(slice);
