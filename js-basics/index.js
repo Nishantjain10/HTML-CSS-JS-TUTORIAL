53236b55e815c4a865d19a931a074aa4f255cb6e
Nishant Jain <nishant.jain.775@gmail.com>
2022-07-10 23:45:04 +0530

    spread operator

--- js-basics/index.js
+++ js-basics/index.js
@@ -881,13 +881,13 @@
 
 //Spread Operator
 
-// const first = [{id: 1}];
-// const second = [4, 5, 6];
+const first = [{id: 1}];
+const second = [4, 5, 6];
 
-// const combined = first.concat(second);
-// first[0].id = 10;
+const combined = first.concat(second);
+first[0].id = 10;
 
 
-// const slice = combined.slice();
-// console.log(combined);
-// console.log(slice);
+const slice = combined.slice();
+console.log(combined);
+console.log(slice);
