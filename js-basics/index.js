b51582c97821c600895fef4b7261eb9b0db33e69
Nishant Jain <nishant.jain.775@gmail.com>
2022-07-11 22:46:14 +0530

    Iterating an Array

--- js-basics/index.js
+++ js-basics/index.js
@@ -888,3 +888,13 @@
 
 // // const copy = combined.slice();
 // const copy = [...combined];
+
+
+
+
+
+//Iterate an array
+const numbers = [1, 2, 3];
+
+for (let number of numbers)
+    console.log(number);
