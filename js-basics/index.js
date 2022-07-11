ef38b1e131570879fa3da33497948f7f4e16b097
Nishant Jain <nishant.jain.775@gmail.com>
2022-07-11 22:51:29 +0530

    Iterating an Array

--- js-basics/index.js
+++ js-basics/index.js
@@ -894,9 +894,9 @@
 
 
 //Iterate an array
-const numbers = [1, 2, 3];
+// const numbers = [1, 2, 3];
 
-for (let number of numbers)
-    console.log(number);
+// for (let number of numbers)
+//     console.log(number);
 
-numbers.forEach((number, index) => console.log(index, number));
+// numbers.forEach((number, index) => console.log(index, number));
