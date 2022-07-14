6e619e84c672714b3c25bf8f23ff500a1a0fd634
Nishant Jain <nishant.jain.775@gmail.com>
2022-07-14 22:57:29 +0530

    Joining an Array In Javascript

--- js-basics/index.js
+++ js-basics/index.js
@@ -912,3 +912,6 @@ console.log(joined);
 const message = 'This is my first message';
 const parts= message.split(' ');
 console.log(parts);
+
+const combined = parts.join('-');
+console.log(combined);
