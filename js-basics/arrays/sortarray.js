c3abc0b5878bb24024efa7378b9d6608c01e07b3
Nishant Jain <nishant.jain.775@gmail.com>
2022-07-15 23:04:57 +0530

    Sorting array In javascript

--- js-basics/arrays/sortarray.js
+++ js-basics/arrays/sortarray.js
@@ -6,18 +6,18 @@
 // console.log(numbers);
 
 
-const courses = [
-    { id:1, courseName:'Node.js'},
-    { id:2, courseName:'javascript'},
-];
+// const courses = [
+//     { id:1, courseName:'Node.js'},
+//     { id:2, courseName:'javascript'},
+// ];
 
-courses.sort(function(a, b) {
-    const nameA = a.courseName.toUpperCase();
-    const nameB = a.courseName.toLowerCase();
+// courses.sort(function(a, b) {
+//     const nameA = a.courseName.toUpperCase();
+//     const nameB = a.courseName.toLowerCase();
 
-    if (nameA < nameB) return -1;
-    if (nameA > nameB) return 1;
-    return 0;
-});
+//     if (nameA < nameB) return -1;
+//     if (nameA > nameB) return 1;
+//     return 0;
+// });
 
-console.log(courses);
+// console.log(courses);
