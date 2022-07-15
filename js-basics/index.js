a6816809c0845be8d8146db4a57bfa31e6b10948
Nishant Jain <nishant.jain.775@gmail.com>
2022-07-15 22:56:53 +0530

    Sorting array In javascript

--- js-basics/index.js
+++ js-basics/index.js
@@ -918,10 +918,20 @@
 
 
 //Sorting an array 
-const numbers = [2, 3, 1];
-numbers.sort();
-console.log(numbers);
+// const numbers = [2, 3, 1];
+// numbers.sort();
+// console.log(numbers);
 
-numbers.reverse();
-console.log(numbers);
+// numbers.reverse();
+// console.log(numbers);
+
+
+const courses = [
+    { id:1, name:'Node.js'},
+    { id:2, name:'Javascript'},
+];
+
+courses.sort();
+
+console.log(courses);
 
