c6b72480629bbf98dc74b148ab976b375653abc1
nishantjain10 <nishant.jain.775@gmail.com>
2022-10-14 21:01:50 +0530

    Filtering an Array

--- js-basics/index.js
+++ js-basics/index.js
@@ -963,3 +963,12 @@
 // const filtered = numbers.filter(n => n >= 0);
 
 // console.log(filtered);
+
+
+
+//Mapping an Array
+const numbers = [1, -1, 2, 3];
+
+const filtered = numbers.filter(n => n >= 0);
+
+console.log(filtered);
