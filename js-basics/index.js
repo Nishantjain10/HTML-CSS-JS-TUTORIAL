8625cc1d51f31f8c01cecbd03f57ed6a52979759
nishantjain10 <nishant.jain.775@gmail.com>
2022-10-14 20:34:57 +0530

    Testing elements of an Array

--- js-basics/index.js
+++ js-basics/index.js
@@ -946,14 +946,11 @@
 // Testing the elements of an Array
 const numbers = [1, -1, 2, 3];
 
+//every()
+//some()
+
 const atLeastOnePositive = numbers.some(function(value) {
     return value >= 0;
 });
-
-
-// const allPositive = numbers.every(function(value) {
-//     return value >= 0;
-// });
-
 
 console.log(atLeastOnePositive);
