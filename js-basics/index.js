cab79e1a29ae80ffa3e1ecf306b49ac0ee2c771e
nishantjain10 <nishant.jain.775@gmail.com>
2022-10-14 20:40:10 +0530

    Filtering an Array

--- js-basics/index.js
+++ js-basics/index.js
@@ -954,3 +954,14 @@
 // });
 
 // console.log(atLeastOnePositive);
+
+
+
+//Filtering an Array 
+const numbers = [1, -1, 2, 3];
+
+const filtered = numbers.filter(function(value) {
+    return value >= 0;
+});
+
+console.log(filtered);
