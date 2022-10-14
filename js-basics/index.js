33d1d4e8363106c593e574a7844d5f4fd90c6c04
nishantjain10 <nishant.jain.775@gmail.com>
2022-10-14 22:35:48 +0530

    Exercise Array from Range

--- js-basics/index.js
+++ js-basics/index.js
@@ -1003,3 +1003,18 @@
 // // sum += n;
 
 // console.log(sum);
+
+
+
+
+//Exercise-1 Array from Range
+const numbers = arrayFromRange(-10, -4);
+
+console.log(numbers);
+
+function arrayFromRange(min,max) {
+    const output = [];
+    for(let i = min; i <= max; i++)
+    output.push(i);
+    return output;
+}
