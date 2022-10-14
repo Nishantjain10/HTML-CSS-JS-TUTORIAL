3d11186fdb9a0e9da0fea3cdc86d286bf7fb8212
nishantjain10 <nishant.jain.775@gmail.com>
2022-10-14 21:34:58 +0530

    Filtering an Array

--- js-basics/index.js
+++ js-basics/index.js
@@ -986,8 +986,9 @@
 //Reducing an Array
 const numbers = [1, -1, 2, 3];
 
-let sum = 0;
-for (let n of numbers)
-sum += n;
+
+const sum = numbers.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+}, 0);
 
 console.log(sum);
