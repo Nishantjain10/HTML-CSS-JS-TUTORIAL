0372b9a465c81673c2862ac22a6ed8dc774629ec
nishantjain10 <nishant.jain.775@gmail.com>
2022-10-14 21:12:03 +0530

    Filtering an Array

--- js-basics/index.js
+++ js-basics/index.js
@@ -971,8 +971,12 @@ const numbers = [1, -1, 2, 3];
 
 const filtered = numbers.filter(n => n >= 0);
 
-const items = filtered.map(n => '<li>' + n + '</li>')
+const items = filtered.map(n => {
+    return obj = { value: n};
+});
 
-const html ='<ul>' + items.join('') + '</ul>';
 
-console.log(html);
+
+// const html ='<ul>' + items.join('') + '</ul>';
+
+console.log(items);
