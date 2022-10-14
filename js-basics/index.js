6dbad2d3b79b02a371bf7a47327b30e706c5ff6c
nishantjain10 <nishant.jain.775@gmail.com>
2022-10-14 20:45:39 +0530

    Filtering an Array

--- js-basics/index.js
+++ js-basics/index.js
@@ -960,8 +960,7 @@
 //Filtering an Array 
 const numbers = [1, -1, 2, 3];
 
-const filtered = numbers.filter(function(value) {
-    return value >= 0;
-});
+const filtered = numbers.filter(n => n >= 0);
+
 
 console.log(filtered);
