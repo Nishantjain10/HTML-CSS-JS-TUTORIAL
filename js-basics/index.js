26b5d0b1fcae63ca830f90be13b5f3e68d48bacb
nishantjain10 <nishant.jain.775@gmail.com>
2022-10-14 22:20:12 +0530

    Reducing an Array

--- js-basics/index.js
+++ js-basics/index.js
@@ -984,22 +984,22 @@
 
 
 //Reducing an Array
-const numbers = [1, -1, 2, 3];
+// const numbers = [1, -1, 2, 3];
 
-// a = 0, c = 1 =>  a = 1
-// a = 1, c = -1 => a = 0
-// a = 0, c = 2 => a = 2
-// a = 2, c = 3 => a = 5
+// // a = 0, c = 1 =>  a = 1
+// // a = 1, c = -1 => a = 0
+// // a = 0, c = 2 => a = 2
+// // a = 2, c = 3 => a = 5
 
-//a = 1, c = -1 => a = 0
-//a = 0, c = 2 => a = 2
-//a = 2, c = 3 => a = 5
-const sum = numbers.reduce(
-    (accumulator, currentValue) => accumulator + currentValue
-);
+// //a = 1, c = -1 => a = 0
+// //a = 0, c = 2 => a = 2
+// //a = 2, c = 3 => a = 5
+// const sum = numbers.reduce(
+//     (accumulator, currentValue) => accumulator + currentValue
+// );
 
-// let sum = 0;
-// for (let n of numbers)
-// sum += n;
+// // let sum = 0;
+// // for (let n of numbers)
+// // sum += n;
 
-console.log(sum);
+// console.log(sum);
