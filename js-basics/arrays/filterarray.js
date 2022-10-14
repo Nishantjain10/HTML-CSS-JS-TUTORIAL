1ad901845a5b9a0b774070df6f66731977b012a5
nishantjain10 <nishant.jain.775@gmail.com>
2022-10-14 20:48:58 +0530

    Filtering an Array

--- js-basics/arrays/filterarray.js
+++ js-basics/arrays/filterarray.js
@@ -1,5 +1,5 @@
-const numbers = [1, -1, 2, 3];
+// const numbers = [1, -1, 2, 3];
 
-const filtered = numbers.filter(n => n >= 0);
+// const filtered = numbers.filter(n => n >= 0);
 
-console.log(filtered);
+// console.log(filtered);
