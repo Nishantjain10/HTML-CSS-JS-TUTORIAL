19667aaff867e16abc25d0b6c0557394f84f3e1d
nishantjain10 <nishant.jain.775@gmail.com>
2022-10-14 20:31:19 +0530

    Testing elements of an Array

--- js-basics/index.js
+++ js-basics/index.js
@@ -942,3 +942,18 @@
 
 // console.log(courses);
 
+
+// Testing the elements of an Array
+const numbers = [1, -1, 2, 3];
+
+const allPositive = numbers.some(function(value) {
+    return value >= 0;
+});
+
+
+// const allPositive = numbers.every(function(value) {
+//     return value >= 0;
+// });
+
+
+console.log(allPositive);
