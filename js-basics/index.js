b3fc1a5f2e19f6095ab338adbeef2b60412eb5c5
nishantjain10 <nishant.jain.775@gmail.com>
2022-10-16 13:48:49 +0530

    Exercise Includes Method

--- js-basics/index.js
+++ js-basics/index.js
@@ -1037,18 +1037,18 @@
 
 //Exercise Except
 
-const numbers = [1, 2, 3, 4, 1 ,1];
+// const numbers = [1, 2, 3, 4, 1 ,1];
 
-const output = except(numbers, [1, 2]);
+// const output = except(numbers, [1, 2]);
 
-console.log(output);
+// console.log(output);
 
-function except(array, excluded) {
-    const output = [];
-    for (let element of array)
-        if (!excluded.includes(element))
-            output.push(element);
-    return output;
-}
+// function except(array, excluded) {
+//     const output = [];
+//     for (let element of array)
+//         if (!excluded.includes(element))
+//             output.push(element);
+//     return output;
+// }
 
 
