2ae836a444448ce6f235d0c6b7b250e35cad6711
nishantjain10 <nishant.jain.775@gmail.com>
2022-10-16 14:53:53 +0530

    Exercise Moving an Element

--- js-basics/index.js
+++ js-basics/index.js
@@ -1101,21 +1101,21 @@
 
 
 //Exercise 6 Get max
-const numbers = [1, 2, 3 ,4];
+// const numbers = [1, 2, 3 ,4];
 
-const max = getMax([1, 2, 2, 1, 3]);
+// const max = getMax([1, 2, 2, 1, 3]);
 
-console.log(max);
+// console.log(max);
 
-function getMax(array) {
-    if (array.length === 0) return undefined;
+// function getMax(array) {
+//     if (array.length === 0) return undefined;
 
-    // let max = array[0];
+//     // let max = array[0];
 
-    // for (let i = 1; i < array.length; i++)
-    //     if(array[i] > max)
-    //         max = array[i];     
-    // return max;
+//     // for (let i = 1; i < array.length; i++)
+//     //     if(array[i] > max)
+//     //         max = array[i];     
+//     // return max;
 
-    return array.reduce((a, b) => (a > b) ? a : b);
-}
+//     return array.reduce((a, b) => (a > b) ? a : b);
+// }
