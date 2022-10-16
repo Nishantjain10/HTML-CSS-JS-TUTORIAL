88327923ff9f9aecde860b59f54f4e7276824042
nishantjain10 <nishant.jain.775@gmail.com>
2022-10-16 13:35:26 +0530

    Exercise Includes Method

--- js-basics/index.js
+++ js-basics/index.js
@@ -1018,3 +1018,20 @@
 //     output.push(i);
 //     return output;
 // }
+
+
+//Exercise-2 Includes
+
+const numbers = [1, 2 , 3, 4];
+
+console.log(includes(numbers, -1));
+
+function includes(array, searchElement) {
+    for (let element of array)
+        if (element === searchElement)
+        return true;
+    return false; 
+    
+}
+
+
