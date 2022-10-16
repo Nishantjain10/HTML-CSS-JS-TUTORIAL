1ab0561a5459df3dd687e879f6bb008ddd9a0532
nishantjain10 <nishant.jain.775@gmail.com>
2022-10-16 14:53:10 +0530

    Exercise Get Max in Array

--- js-basics/index.js
+++ js-basics/index.js
@@ -1098,3 +1098,24 @@
 //     }, 0);
 // }
 
+
+
+//Exercise 6 Get max
+const numbers = [1, 2, 3 ,4];
+
+const max = getMax([1, 2, 2, 1, 3]);
+
+console.log(max);
+
+function getMax(array) {
+    if (array.length === 0) return undefined;
+
+    // let max = array[0];
+
+    // for (let i = 1; i < array.length; i++)
+    //     if(array[i] > max)
+    //         max = array[i];     
+    // return max;
+
+    return array.reduce((a, b) => (a > b) ? a : b);
+}
