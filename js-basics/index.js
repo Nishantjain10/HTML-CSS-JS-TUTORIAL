96df9067f9aec986ac09f3128aa5bb35e2a44c31
nishantjain10 <nishant.jain.775@gmail.com>
2022-10-16 14:14:07 +0530

    Exercise Moving an Element

--- js-basics/index.js
+++ js-basics/index.js
@@ -1055,22 +1055,22 @@
 
 //Exercise Moving an Element;
 
-const numbers = [1, 2, 3, 4];
+// const numbers = [1, 2, 3, 4];
 
-const output = move(numbers, 1, 2);
+// const output = move(numbers, 1, 2);
 
-console.log(output);
+// console.log(output);
 
-function move(array, index, offset) {
-    const position = index + offset;
-    if(position >= array.length || position < 0) { 
-         console.error('Invalid offset.');
-         return;
-    }
+// function move(array, index, offset) {
+//     const position = index + offset;
+//     if(position >= array.length || position < 0) { 
+//          console.error('Invalid offset.');
+//          return;
+//     }
 
-    const output = [...array];
-    const element = output.splice(index, 1)[0];
-    output.splice(index + offset, 0, element);
-    return output;
-}
+//     const output = [...array];
+//     const element = output.splice(index, 1)[0];
+//     output.splice(index + offset, 0, element);
+//     return output;
+// }
 
