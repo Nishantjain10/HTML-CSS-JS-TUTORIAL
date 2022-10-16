015965fe86fce9b44e5c96ca5e3e47439d715ae2
nishantjain10 <nishant.jain.775@gmail.com>
2022-10-16 14:37:21 +0530

    Exercise Counting Occurrences

--- js-basics/index.js
+++ js-basics/index.js
@@ -1074,3 +1074,27 @@
 //     return output;
 // }
 
+
+
+//Exercise Count occurences
+const numbers = [1, 2, 3, 4, 1];
+
+const count = countOccurences(numbers, 1);
+
+console.log(count);
+
+function countOccurences(array, searchElement) {
+    // let count = 0;
+    // for (let element of array)
+    //     if(element === searchElement)
+    //         count++;
+    // return count;
+
+
+    return array.reduce((accumulator, current) => {
+        const occurences = (current === searchElement) ? 1 : 0;
+        console.log(accumulator, current, searchElement);
+        return accumulator + occurences;
+    }, 0);
+}
+
