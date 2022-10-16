fb0bcb1401dd2553fafc65ec5291d17879c337d4
nishantjain10 <nishant.jain.775@gmail.com>
2022-10-16 13:48:08 +0530

    Exercise Except Method

--- js-basics/index.js
+++ js-basics/index.js
@@ -1031,7 +1031,24 @@
 //         if (element === searchElement)
 //         return true;
 //     return false; 
-    
 // }
+
+
+
+//Exercise Except
+
+const numbers = [1, 2, 3, 4, 1 ,1];
+
+const output = except(numbers, [1, 2]);
+
+console.log(output);
+
+function except(array, excluded) {
+    const output = [];
+    for (let element of array)
+        if (!excluded.includes(element))
+            output.push(element);
+    return output;
+}
 
 
