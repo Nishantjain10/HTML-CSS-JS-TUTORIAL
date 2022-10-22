16af20e727b197f3bab242003cdeada865cd7371
nishantjain10 <nishant.jain.775@gmail.com>
2022-10-22 22:49:33 +0530

    Arguments in Javascript

---
+++ js-basics/functions/argument.js
@@ -0,0 +1,11 @@
+//function sum() {
+    //     let total = 0;
+    //     for (let value of arguments)
+    //         total += value;
+    //     return total;
+    
+    //     console.log(arguments);
+    //     return a + b;
+    // }
+    
+    // console.log(sum(1, 2 , 3, 4, 5));
--- js-basics/index.js
+++ js-basics/index.js
@@ -1169,16 +1169,16 @@
 
 
 //Arguments
-function sum() {
-    let total = 0;
-    for (let value of arguments)
-        total += value;
-    return total;
+// function sum() {
+//     let total = 0;
+//     for (let value of arguments)
+//         total += value;
+//     return total;
 
-    console.log(arguments);
-    return a + b;
-}
+//     console.log(arguments);
+//     return a + b;
+// }
 
-console.log(sum(1, 2 , 3, 4, 5));
+// console.log(sum(1, 2 , 3, 4, 5));
 
 
