15445b2693e6ec8d7fcb35e7164c79f038573dec
nishantjain10 <nishant.jain.775@gmail.com>
2022-10-22 21:10:35 +0530

    Functions in Javascript(Functions Expression and Declaration)

--- js-basics/index.js
+++ js-basics/index.js
@@ -1145,3 +1145,25 @@
 //  .map(m => m.title)
 
 // console.log(titles);
+
+
+//FUNCTION IN JS
+
+//Function Declaration 
+    function walk() {
+    console.log('walk');
+}
+
+//Named Function Expression
+// let run = function(walk) {
+//     console.log('run');
+// };
+
+//Anonymous Function Expression
+let run = function() {
+    console.log('run');
+};
+let move = run;
+run();
+move()  
+
