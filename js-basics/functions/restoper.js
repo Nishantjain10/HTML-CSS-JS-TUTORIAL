462dba16a782cca0819029e2b8084f605a4cbc81
nishantjain10 <nishant.jain.775@gmail.com>
2022-10-23 11:04:25 +0530

    Rest Operator in JS

---
+++ js-basics/functions/restoper.js
@@ -0,0 +1,6 @@
+//function sum(discount, ...prices) {
+    //     const total = prices.reduce((a, b) => a + b);
+    //     return total * (1 - discount);
+    // }
+    
+    // console.log(sum(0.1, 20, 30));
--- js-basics/index.js
+++ js-basics/index.js
@@ -1184,12 +1184,12 @@
 
 
 //Rest Operator
-function sum(discount, ...prices) {
-    const total = prices.reduce((a, b) => a + b);
-    return total * (1 - discount);
-}
+// function sum(discount, ...prices) {
+//     const total = prices.reduce((a, b) => a + b);
+//     return total * (1 - discount);
+// }
 
-console.log(sum(0.1, 20, 30));
+// console.log(sum(0.1, 20, 30));
 
 
 
