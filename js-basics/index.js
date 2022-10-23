21d6fd840080e9a4ea0b03215bcfca75d33c62dd
nishantjain10 <nishant.jain.775@gmail.com>
2022-10-23 11:54:02 +0530

    Rest Operator in JS

--- js-basics/index.js
+++ js-basics/index.js
@@ -1198,7 +1198,7 @@ function interest(principal, rate = 3.5, years = 5) {
     return principal * rate / 100 * years;
 }
 
-console.log(interest(10000))
+console.log(interest(10000, undefined, 5))
 
 
 
