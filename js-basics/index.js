651835fd6c9d054582f10a8b17857ba0d8fe5f03
nishantjain10 <nishant.jain.775@gmail.com>
2022-10-23 10:59:28 +0530

    Rest Operator in JS

--- js-basics/index.js
+++ js-basics/index.js
@@ -1182,3 +1182,15 @@
 // console.log(sum(1, 2 , 3, 4, 5));
 
 
+
+//Rest Operator
+function sum(...args) {
+    return args.reduce((a, b) => a + b);
+}
+
+console.log(sum(1, 2 , 3, 4, 5, 10));
+
+
+
+
+
