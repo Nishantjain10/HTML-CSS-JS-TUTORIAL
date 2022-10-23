145769c0671e62c22bc547c90e73e9400e778285
nishantjain10 <nishant.jain.775@gmail.com>
2022-10-23 20:52:02 +0530

    let vs var In JS

--- js-basics/index.js
+++ js-basics/index.js
@@ -1280,14 +1280,27 @@
 
 //Let vs Var
 
-function start() {
-    for (var i = 0; i < 5; i++)    
-        console.log(i);
+// function start() {
+//     for (var i = 0; i < 5; i++) {
+//         if (true) {
+//             var color = 'red';
+//         }
+//     }          
 
-    console.log(i);
-}
+//     console.log(color);
+// }
+// start();
 
 //var - was used to define constants  => function-scoped
 //ES6(ES2015) - let, const => block-scoped
 
-start();
+var color = 'red';
+let age = 30; 
+
+function sayHi() {
+    console.log('Hi')
+}
+
+//avoid adding stuff to window object
+//avoid using var 
+
