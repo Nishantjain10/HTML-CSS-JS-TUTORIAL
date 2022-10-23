cd2a10ba524d2ac39ddde4cc8477f1482707393e
nishantjain10 <nishant.jain.775@gmail.com>
2022-10-23 14:12:11 +0530

    Getters and Setters

---
+++ js-basics/functions/getset.js
@@ -0,0 +1,19 @@
+// const person = {
+//     firstName: 'Nishant',
+//     lastName: 'Jain',
+//     get fullName() {
+//         return `${person.firstName} ${person.lastName}`;
+//     },
+//     set fullName(value) {
+//         const parts = value.split(' ');
+//         this.firstName = parts[0];
+//         this.lastName = parts[1];
+//     }
+// };
+
+// person.fullName = 'John Smith';
+
+// // getters => access properties
+// // setters => change(mutate) them
+
+// console.log(person);
--- js-basics/index.js
+++ js-basics/index.js
@@ -1204,25 +1204,25 @@
 
 
 //Getters and Setters
-const person = {
-    firstName: 'Nishant',
-    lastName: 'Jain',
-    get fullName() {
-        return `${person.firstName} ${person.lastName}`;
-    },
-    set fullName(value) {
-        const parts = value.split(' ');
-        this.firstName = parts[0];
-        this.lastName = parts[1];
-    }
-};
+// const person = {
+//     firstName: 'Nishant',
+//     lastName: 'Jain',
+//     get fullName() {
+//         return `${person.firstName} ${person.lastName}`;
+//     },
+//     set fullName(value) {
+//         const parts = value.split(' ');
+//         this.firstName = parts[0];
+//         this.lastName = parts[1];
+//     }
+// };
 
-person.fullName = 'John Smith';
+// person.fullName = 'John Smith';
 
-// getters => access properties
-// setters => change(mutate) them
+// // getters => access properties
+// // setters => change(mutate) them
 
-console.log(person);
+// console.log(person);
 
 
 
