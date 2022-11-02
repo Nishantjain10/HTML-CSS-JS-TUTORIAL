a79bf8bdc759d908078930e28588cb23c5534327
nishantjain10 <nishant.jain.775@gmail.com>
2022-11-02 16:50:41 +0530

    Exercise Error Handling

--- js-basics/index.js
+++ js-basics/index.js
@@ -1389,3 +1389,27 @@
 // };
 
 // console.log(circle.area);
+
+
+
+//Error Handling Exercise
+try{ 
+const numbers = [1, 2, 3, 4, 1];
+const count = countOccurrences(null, 1);
+console.log(count);
+}
+
+catch (e) {
+    console.log(e.message);
+}
+
+function countOccurrences(array, searchElement) {
+    if (!Array.isArray(array))
+        throw new Error ('Invalid array.');
+
+    return array.reduce((accumulator, current) => {
+        const occurrences = (current === searchElement) ? 1 : 0;
+        console.log(accumulator, current, searchElement);
+        return accumulator + occurrences;
+    }, 0);
+}
