fd2112d20f109cef600395e41d6257c4382c853a
nishantjain10 <nishant.jain.775@gmail.com>
2022-11-02 14:36:22 +0530

    Exercise Sum Of Arguments (With Array Input)

--- js-basics/index.js
+++ js-basics/index.js
@@ -1366,11 +1366,11 @@
 
 // sum(1, 2, 3 ,4) => 10
 
-console.log(sum([1, 2, 3, 4]))
+// console.log(sum([1, 2, 3, 4]))
 
-function sum(...items) {
-    if (items.length === 1 && Array.isArray(items[0]))
-        items = [...items[0]];
+// function sum(...items) {
+//     if (items.length === 1 && Array.isArray(items[0]))
+//         items = [...items[0]];
 
-   return items.reduce((a, b) => a + b);
-}
+//    return items.reduce((a, b) => a + b);
+// }
