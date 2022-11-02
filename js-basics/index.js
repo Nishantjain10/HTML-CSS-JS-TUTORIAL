6e13930bb6d9ebe8029702c80b4b82710eca009f
nishantjain10 <nishant.jain.775@gmail.com>
2022-11-02 14:34:56 +0530

    Exercise Sum Of Arguments (With Array Input)

--- js-basics/index.js
+++ js-basics/index.js
@@ -1358,3 +1358,19 @@
 // playVideo.bind({ name: 'Nishant'})();
 
 // playVideo();
+
+
+
+
+//Exercise 1 Sum of Arguments
+
+// sum(1, 2, 3 ,4) => 10
+
+console.log(sum([1, 2, 3, 4]))
+
+function sum(...items) {
+    if (items.length === 1 && Array.isArray(items[0]))
+        items = [...items[0]];
+
+   return items.reduce((a, b) => a + b);
+}
