fd29b67e831bf1c6fbed4914cf7c83aafe2f8db6
nishantjain10 <nishant.jain.775@gmail.com>
2022-11-02 13:46:42 +0530

    The 'This' keyword in javascript

---
+++ js-basics/functions/this.js
@@ -0,0 +1,23 @@
+// // The this Keyword In javascript
+// // method --> obj
+// //function -> global (window, global)
+
+// const video = {
+//     title: 'a',
+//     tags: ['a', 'b', 'c'],
+//     showTags() {
+//         this.tags.forEach(function(tag) {
+//             console.log(this, tag);
+//         }, this);
+//     }
+// };
+
+// video.showTags();
+
+
+// // function Video(title) {
+// //     this.title = title;
+// //     console.log(this);
+// // } 
+  
+// // const v = new Video('b'); //{}
--- js-basics/index.js
+++ js-basics/index.js
@@ -1308,26 +1308,26 @@
 
 
 
-// The this Keyword In javascript
-// method --> obj
-//function -> global (window, global)
+// // The this Keyword In javascript
+// // method --> obj
+// //function -> global (window, global)
 
-const video = {
-    title: 'a',
-    tags: ['a', 'b', 'c'],
-    showTags() {
-        this.tags.forEach(function(tag) {
-            console.log(this, tag);
-        }, this);
-    }
-};
+// const video = {
+//     title: 'a',
+//     tags: ['a', 'b', 'c'],
+//     showTags() {
+//         this.tags.forEach(function(tag) {
+//             console.log(this, tag);
+//         }, this);
+//     }
+// };
 
-video.showTags();
+// video.showTags();
 
 
-// function Video(title) {
-//     this.title = title;
-//     console.log(this);
-// } 
+// // function Video(title) {
+// //     this.title = title;
+// //     console.log(this);
+// // } 
   
-// const v = new Video('b'); //{}
+// // const v = new Video('b'); //{}
