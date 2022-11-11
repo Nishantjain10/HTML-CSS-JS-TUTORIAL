f40c559b1ed88d9de526279f031527cd25883615
nishantjain10 <nishant.jain.775@gmail.com>
2022-11-11 11:42:19 +0530

    Passenger Counter In Java Script

---
+++ Scrimba-JavaScript/index.js
@@ -0,0 +1,74 @@
+// document.getElementById("count").innerText = 5
+
+
+// userPoints = 50;
+
+// bonusUserPoints = userPoints + 50;
+
+// bonusUserPoints = userPoints - 25;
+
+// bonusUserPoints = userPoints + 70;
+
+// console.log(bonusUserPoints);
+
+// function increment() {
+//     console.log('The button is CLICKED!')
+// }
+
+
+// function printNumber() {
+//     console.log('42')
+// }
+
+
+// lapOne = 32;
+// lapTwo = 34;
+// lapThree = 36;
+
+// function lapCount() {
+//     console.log(lapOne + lapTwo + lapThree);   
+// }
+
+// lapCount()
+
+// let lapsCompleted = 0;
+
+// function lapIncrement() {
+//     lapsCompleted = lapsCompleted + 1
+// }
+
+// lapIncrement()
+// lapIncrement()
+// lapIncrement()
+
+// console.log(lapsCompleted)
+
+
+let countEl = document.getElementById("count-el")
+console.log(countEl)
+
+let count = 0;
+
+function increment() {
+    count = count + 1;
+    countEl.innerText = count;
+    console.log(count)
+}
+
+
+
+function decrement() {
+    count = count - 1;
+    countEl.innerText = count;
+    console.log(count)
+}
+
+
+
+function save() {
+    console.log(count)
+}
+console.log(myGreeting)
+
+
+
